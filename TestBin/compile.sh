c85cc96ddf0946d0150fc8106e6b7f025cc82abc
HunarPG <HunarSangwanPG@gmail.com>
2023-09-30 19:04:57 +0530

    Fixed Last commit

--- TestBin/compile.sh
+++ TestBin/compile.sh
@@ -4,7 +4,7 @@ echo ../Gen3Predictor.jar
 rm ../Gen3Predictor.jar 
 
 echo Compiling Java code
-javac -cp /home/jacob/git/Pokemon-Gen3-IV-Predictor/src -d /home/jacob/git/Pokemon-Gen3-IV-Predictor/TestBin/ /home/jacob/git/Pokemon-Gen3-IV-Predictor/src/gen3check/Main.java
+javac -cp ../src -d ../TestBin/ ../src/gen3check/Main.java
 
 echo Building into jar file
 jar cfe ../Gen3Predictor.jar gen3check.Main ./gen3check/* ./gen3check/gui/* ./gen3check/observers/* ./gen3check/pokemon/* ./gen3check/predictor/* ./gen3check/util/* ./rng/* ./database/* ./font/* ./image/*
